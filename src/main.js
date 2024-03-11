06999e238aa8edd6ae1a8648a073e4589856f8dc
zhoushun <229591451@qq.com>
2024-03-11 11:03:23 +0800

    update: 增加表单生成器菜单模块

--- src/main.js
+++ src/main.js
@@ -6,7 +6,12 @@ import pinia from './pinia/index.js'
 import directive from './directive/index.js'
 import i18n from './lang/index.js'
 
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
+
+import VForm3 from 'vform3-builds'
+import 'vform3-builds/dist/designer.style.css'
 
 // 注册SVG图标
 import 'virtual:svg-icons-register'
@@ -20,6 +25,8 @@ app.use(router)
 app.use(pinia)
 app.use(directive)
 app.use(i18n)
+app.use(ElementPlus)
+app.use(VForm3)
 app.use(gloablComponents)
 app.mount('#app')
 
