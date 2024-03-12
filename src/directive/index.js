ff3174f0db66907124575c7a3fc8d8cc524e7e06
zhoushun <229591451@qq.com>
2024-03-12 09:26:52 +0800

    update: 拖拽指令

--- src/directive/index.js
+++ src/directive/index.js
@@ -2,12 +2,14 @@ import perms from './modules/perms.js'
 import role from './modules/role.js'
 import copy from './modules/copy.js'
 import watermark from './modules/watermark.js'
+import draggable from './modules/draggable.js'
 
 const directives = {
   perms,
   role,
   copy,
-  watermark
+  watermark,
+  draggable
 }
 
 export default {
