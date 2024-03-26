2390ec0dbda4769cf8f5c76941ce2f6a1928ae74
zhoushun <229591451@qq.com>
2024-03-26 10:27:20 +0800

    update: 增加引导页插件driver.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,12 @@
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import SvgIcon from './SvgIcon/SvgIcon.vue'
 import WangEditor from './WangEditor/WangEditor.vue'
+import StepGuide from './StepGuide/StepGuide.vue'
 
 const components = {
 	SvgIcon,
-	WangEditor
+	WangEditor,
+	StepGuide,
 }
 export default {
 	install(app) {
