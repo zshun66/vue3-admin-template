b87a2afd72fb30fccc18f43c3f6a71316c01c40a
zhoushun <229591451@qq.com>
2024-03-28 15:12:41 +0800

    update: 增加拖拽插件vuedraggable

--- src/views/backstage-pages/dashboard/workbenches/workbenches.vue
+++ src/views/backstage-pages/dashboard/workbenches/workbenches.vue
@@ -1,14 +1,8 @@
 <script setup name="backstage:dashboard:workbenches">
-const showImportExcel = ref(false)
 </script>
 
 <template>
-  <div class="page_container workbenches_page">
-    <el-button type="primary" @click="showImportExcel = true">导入数据</el-button>
-    <ImportExcel v-model="showImportExcel">
-      <template #empty>按时发生</template>
-    </ImportExcel>
-  </div>
+  <div class="page_container workbenches_page">工作台</div>
 </template>
 
 <style scoped lang="scss"></style>
